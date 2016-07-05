87bb410c4f81f5949b09f4391fcfcf7d694c1437
Yuhui <276635769@qq.com>
2016-07-05 21:03:59 +0800

    增加退出

--- app/templates/js/index.js
+++ app/templates/js/index.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
    		var username = ret.username;
    		if(islogined) {
             $("#sign_state").append('<a href="userinfo" class="button button-primary button-small">欢迎您, ' + username + '</a>');
+            $("#sign_state").append('<a href="register" id="quit_button" class="button button-action button-small">退出</a>');
          }
    		else {
             $("#sign_state").append('<a href="login" class="button button-primary button-small">登录</a>');
@@ -12,3 +13,14 @@ $(document).ready(function() {
          }
   	});
 })
+
+$("#quit_button").click(function() {
+   $.post("api/islogined",
+   {
+      "islogined" : false,
+   },
+   function(data) {
+      window.location.reload(); 
+   }            
+});
+
