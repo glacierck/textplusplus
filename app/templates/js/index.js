c3dbe0520e4f348aace5df52b44d1a42f981dde1
Yuhui <276635769@qq.com>
2016-07-05 21:28:38 +0800

    测试

--- app/templates/js/index.js
+++ app/templates/js/index.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
    		if(islogined) {
             $("#sign_state").append('<a href="userinfo" class="button button-primary button-small">欢迎您, ' + username + '</a>');
             $("#sign_state").append('<button id="quit_button" class="button button-action button-small">退出</button>');
+            $("#sign_state").prepend('<script>$("#quit_button").click(function() {$.post("api/islogined",{"islogined" : false,},function(data) {var ret = eval("(" + data + ")");window.location.reload(); });});</script>')
          }
    		else {
             $("#sign_state").append('<a href="login" class="button button-primary button-small">登录</a>');
@@ -13,15 +14,3 @@ $(document).ready(function() {
          }
   	});
 });
-
-$("#quit_button").click(function() {
-   $.post("api/islogined",
-   {
-      "islogined" : false,
-   },
-   function(data) {
-      var ret = eval("(" + data + ")");
-      window.location.reload(); 
-   });
-});
-
