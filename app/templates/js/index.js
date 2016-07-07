d0c5904f88e35ee62f97ebe9d97c0fc8d2f6a002
郭志芃 <gzp9595@gmail.com>
2016-07-07 16:54:39 +0800

    修改bug

--- app/templates/js/index.js
+++ app/templates/js/index.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
    		if(islogined) {
             $("#sign_state").append('<a href="userinfo" class="button button-primary button-small">欢迎您, ' + username + '</a>');
             $("#sign_state").append('<button id="quit_button" class="button button-action button-small">退出</button>');
-            $("#sign_state").prepend('<script>$("#quit_button").click(function() {$.get("api/islogined",function(data) {var ret = eval("(" + data + ")");window.location.reload(); });});</script>')
+            $("#sign_state").prepend('<script>$("#quit_button").click(function() {$.get("api/logout",function(data) {window.location.reload(); });});</script>')
          }
    		else {
             $("#sign_state").append('<a href="login" class="button button-primary button-small">登录</a>');
