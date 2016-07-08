57566c5f69a5cc7644e8993d2779a184e90ee36e
Yuhui <276635769@qq.com>
2016-07-08 19:11:44 +0800

    可以运行

--- app/algorithm/thulac/__init__.py
+++ app/algorithm/thulac/__init__.py
@@ -1,11 +1,11 @@
 from ctypes import cdll,c_char_p,string_at
-lib = cdll.LoadLibrary('app/algorithm/thulac/thulac.dll')
+#lib = cdll.LoadLibrary('app/algorithm/thulac/thulac.dll')
 
 class Thulac(object):
 	def __init__(self):
-		self.obj = lib.Thulac_new()
-		lib.Thulac_run.restype = c_char_p
-
+		#self.obj = lib.Thulac_new()
+		#lib.Thulac_run.restype = c_char_p
+		pass
 	def run(self,raw):
 		lib.Thulac_run(self.obj,raw)
 
