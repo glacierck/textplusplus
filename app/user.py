aa4f180061f1819244452d18640ca9162a7a3951
郭志芃 <gzp9595@gmail.com>
2016-07-15 10:13:22 +0800

    bug

--- app/user.py
+++ app/user.py
@@ -38,9 +38,9 @@ def register():
 
 	t = time.time()
 	dt = 'd'+time.strftime('%Y%m%d',time.localtime(time.time()))
-	cursor.execute("INSERT INTO user(id, email, token, admin, password, create_at, last_login, token_time) VALUES('%s','%s','%s','%s',%s,%s,%s,%s)", (uid, uemail, token(), False, user['password'].encode('utf-8'), t, t, t))
-	cursor.execute("INSERT INTO lac(user,%s) VALUES('%s','%s')", (dt,uid,'0'))
-	cursor.execute("INSERT INTO tokeninfo(user, lac_limit_times, lac_last_time, lac_frequen) VALUES('%s',%s,%s,%s)", (uid, 100, 0, 0.1))
+	cursor.execute("INSERT INTO user(id, email, token, admin, password, create_at, last_login, token_time) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)", (uid, uemail, token(), False, user['password'].encode('utf-8'), t, t, t))
+	cursor.execute("INSERT INTO lac(user,%s) VALUES(%s,%s)" % (dt), (uid,'0'))
+	cursor.execute("INSERT INTO tokeninfo(user, lac_limit_times, lac_last_time, lac_frequen) VALUES(%s,%s,%s,%s)", (uid, 100, 0, 0.1))
 
 	conn.commit()
 	conn.close()
@@ -68,7 +68,7 @@ def log_in():
 		return json.dumps({ 'code': 307, 'message': 'the password is wrong' }), 203
 	
 	t = time.time()
-	cursor.execute("update user set last_login = '%s' where id = '%s'", (t,uid))
+	cursor.execute("update user set last_login = %s where id = %s", (t,uid))
 	conn.commit()
 	conn.close()
 
