ad4de051ad30989de050b10e560d65b62da1d349
郭志芃 <gzp9595@gmail.com>
2016-07-18 14:56:09 +0800

    修改 bug

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import MySQLdb
 import time
 conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='thunlp',port=3306)
 conn.select_db('thunlp')
-
+cursor=conn.cursor()
 dt = 'd'+time.strftime('%Y%m%d',time.localtime(time.time()))
 cursor.execute('alter table lac add column ' + dt + ' integer;')
 conn.commit()
