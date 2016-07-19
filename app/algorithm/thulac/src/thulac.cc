e17795becda1ff5a11710f35dc5751d8262c85a2
郭志芃 <gzp9595@gmail.com>
2016-07-19 14:52:12 +0800

    no message

--- app/algorithm/thulac/src/thulac.cc
+++ app/algorithm/thulac/src/thulac.cc
@@ -309,7 +309,7 @@ public:
                     ret = ret + segged[j].getString();
                 }
             }
-            printf("%s\n",ret);
+            printf("%s\n",ret.c_str());
             return ret.c_str();
             //cout<<endl;
         }
