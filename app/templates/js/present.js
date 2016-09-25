f7989853b78c5adada4fa550f51a230ec53471dd
Yuhui <276635769@qq.com>
2016-09-25 18:50:59 +0800

    thucke实现

--- app/templates/js/present.js
+++ app/templates/js/present.js
@@ -98,3 +98,67 @@ $(document).ready(function() {
     });
     }
 })
+
+$(document).ready(function() {
+    $("#extract_button").click(function(){
+        $.post("api/thucke",
+        {
+            "token" : "qC7pRO8LH912194p36DH",
+            "text": $("#input_sec3").val(),
+        },
+        function(data) {
+            var word_list = eval("(" + data + ")");
+            $(function() {
+                $("#extract_container").jQCloud(word_list);
+            });
+        });
+        //draw_extract_chart();
+    });
+    // function draw_extract_chart() {
+    //     var word_list = [
+    //     {text: "Lorem", weight: 13},
+    //     {text: "Ipsum", weight: 10.5},
+    //     {text: "Dolor", weight: 9.4},
+    //     {text: "Sit", weight: 8},
+    //     {text: "Amet", weight: 6.2},
+    //     {text: "Consectetur", weight: 5},
+    //     {text: "Adipiscing", weight: 5},
+    //     {text: "Elit", weight: 5},
+    //     {text: "Nam et", weight: 5},
+    //     {text: "Leo", weight: 4},
+    //     {text: "Sapien", weight: 4},
+    //     {text: "Pellentesque", weight: 3},
+    //     {text: "habitant", weight: 3},
+    //     {text: "morbi", weight: 3},
+    //     {text: "tristisque", weight: 3},
+    //     {text: "senectus", weight: 3},
+    //     {text: "et netus", weight: 3},
+    //     {text: "et malesuada", weight: 3},
+    //     {text: "fames", weight: 2},
+    //     {text: "ac turpis", weight: 2},
+    //     {text: "egestas", weight: 2},
+    //     {text: "Aenean", weight: 2},
+    //     {text: "vestibulum", weight: 2},
+    //     {text: "elit", weight: 2},
+    //     {text: "sit amet", weight: 2},
+    //     {text: "metus", weight: 2},
+    //     {text: "adipiscing", weight: 2},
+    //     {text: "ut ultrices", weight: 2},
+    //     {text: "justo", weight: 1},
+    //     {text: "dictum", weight: 1},
+    //     {text: "Ut et leo", weight: 1},
+    //     {text: "metus", weight: 1},
+    //     {text: "at molestie", weight: 1},
+    //     {text: "purus", weight: 1},
+    //     {text: "Curabitur", weight: 1},
+    //     {text: "diam", weight: 1},
+    //     {text: "dui", weight: 1},
+    //     {text: "ullamcorper", weight: 1},
+    //     {text: "id vuluptate ut", weight: 1},
+    //     {text: "mattis", weight: 1},
+    //     {text: "et nulla", weight: 1},
+    //     {text: "Sed", weight: 1}
+    //   ];
+      
+    // }
+})
